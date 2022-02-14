49fa3711afde790b07be379ef5eeb26f8ea87bbf
tosalmanshaikh <tosalmanshaikh@gmail.com>
2022-02-14 11:54:22 +0100

    Gallery-updated

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,43 +1,44 @@
 import React from 'react'
 
-import Muffins from '../../images/photos /BlueberryMuffin.JPG';
-import SpecialCroissants from '../../images/photos /SpecialCroissantsHero.JPG'
 import BananaBread from '../../images/photos /BananaBread.JPG';
-import Coffee from '../../images/photos /coffee1.jpg';
-import SourDough from '../../images/photos /SourdoughNatural.JPG'
-import PecanCoffeeCake from '../../images/photos /PecanCoffeeCake.JPG'
-import BrownieWalnuts from '../../images/photos /BrownieWalnuts.JPG'
-import SourDoughHero from '../../images/photos /SourdoughNaturalHero.JPG'
+import ChilliDarkChocolate from '../../images/photos /ChilliDarkChocolate.jpeg'
+import SourDoughHero from '../../images/photos /SourdoughNaturalHero.JPG';
+import TomatoesAuberginePinenuts from '../../images/photos /TomatoesAuberginePinenuts.jpg';
+import Coffee1 from '../../images/photos /coffee1.jpg';
+import SpecialCroissants from '../../images/photos /SpecialCroissantsHero.JPG'
+import Coffee2 from '../../images/photos /coffee2.jpg';
+import CheesecakeRedfruits from '../../images/photos /CheeseCakeRedFruits.jpeg';
 
 
-
-export default function Gallery() {
+function Gallery() {
     return (
         <div className="Gallery HomePageGallery">
             <figure className="Gallery__Item--1">
-                <img src={Muffins} alt="Multi Tier cake" className="Gallery__img" />
+                <img src={BananaBread} alt="Multi Tier cake" className="Gallery__img"/>
             </figure>
             <figure className="Gallery__Item--2">
-                <img src={BananaBread} alt="Stacked Donuts" className="Gallery__img" />
+                <img src={ChilliDarkChocolate} alt="Stacked Donuts" className="Gallery__img"/>
             </figure>
             <figure className="Gallery__Item--3">
-                <img src={SourDoughHero} alt="Stacked Donuts" className="Gallery__img" />
+                <img src={SourDoughHero} alt="Stacked Donuts" className="Gallery__img"/>
             </figure>
             <figure className="Gallery__Item--4">
-                <img src={BrownieWalnuts} alt="Stacked Donuts" className="Gallery__img" />
+                <img src={TomatoesAuberginePinenuts} alt="Stacked Donuts" className="Gallery__img"/>
             </figure>
             <figure className="Gallery__Item--5">
-                <img src={Coffee} alt="Stacked Donuts" className="Gallery__img" />
+                <img src={Coffee1} alt="Stacked Donuts" className="Gallery__img"/>
             </figure>
             <figure className="Gallery__Item--6">
-                <img src={PecanCoffeeCake} alt="Stacked Donuts" className="Gallery__img" />
+                <img src={SpecialCroissants} alt="Stacked Donuts" className="Gallery__img"/>
             </figure>
             <figure className="Gallery__Item--7">
-                <img src={SpecialCroissants} alt="Stacked Donuts" className="Gallery__img" />
+                <img src={Coffee2} alt="Stacked Donuts" className="Gallery__img"/>
             </figure>
             <figure className="Gallery__Item--8">
-                <img src={SourDough} alt="Stacked Donuts" className="Gallery__img" />
+                <img src={CheesecakeRedfruits} alt="Stacked Donuts" className="Gallery__img"/>
             </figure>
         </div>
     )
 }
+
+export default Gallery;
